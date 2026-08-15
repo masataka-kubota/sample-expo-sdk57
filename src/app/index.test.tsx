a3de32f7e0ef3e771f7e2f4e7e061b0a9f58a3f2
Masataka <mezase226334@gmail.com>
2026-08-16 06:44:00 +0900

    test: enhance <Index /> screen test to use screen for element selection

--- src/app/index.test.tsx
+++ src/app/index.test.tsx
@@ -1,11 +1,11 @@
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 
 import Index from '@/app/index';
 
 describe('<Index />', () => {
   test('Text renders correctly on HomeScreen', async () => {
-    const { getByText } = await render(<Index />);
+    await render(<Index />);
 
-    getByText('Edit src/app/index.tsx to edit this screen.');
+    expect(screen.getByText('Edit src/app/index.tsx to edit this screen.')).toBeTruthy();
   });
 });
